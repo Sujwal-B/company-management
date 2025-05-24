69ef73d19539f62698793153b8455ff8b49da987
Sujwal Boppana <sujwal.b@zeroco.de>
2025-05-24 14:12:32 +0530

    Update index.js

--- company-management-frontend/src/index.js
+++ company-management-frontend/src/index.js
@@ -6,9 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
 import { NotificationProvider } from './context/NotificationContext'; // Import NotificationProvider
 
-import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -16,8 +13,6 @@ root.render(
       <NotificationProvider> {/* Wrap App with NotificationProvider */}
         <App />
       </NotificationProvider>
-    <AuthProvider> {/* Wrap App with AuthProvider */}
-      <App />
     </AuthProvider>
   </React.StrictMode>
 );
